85de66317c4ec242bc6c671d3c5de1d1573a6228
quang luan <hoangquangluan23102001@gmail.com>
2024-02-02 15:23:44 +0700

    khok2

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,10 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   proxy: true,
   url: env("MY_RAILWAY_URL"),
   app: {
     keys: env.array("APP_KEYS"),
+  },
+  webhooks: {
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
